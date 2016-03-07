b2cd4f48271dcd1f1656f4cfa29e56510cbe21bd
Sven Brettschneider <Sven Brettschneider>
2016-03-07 19:44:16 +0100

    opzetten basis phonegap applicatie met lijst van pokedex API

--- www/js/index.js
+++ www/js/index.js
@@ -1,21 +1,33 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+var POKEDEX_REST_PREFIX_URL = "http://pokeapi.co/api/v2/";
+
+$(document).on("pagecreate", "#home", function(){
+  $('#mainview').on('tap', '.pokemon-list', function(){
+    //event voor wanneer je op een pokemon item clickt
+  });
+
+  refreshAllPokemonList();
+});
+
+function refreshAllPokemonList(){
+  $.getJSON(POKEDEX_REST_PREFIX_URL + "pokemon?limit=151", function( data ) {
+    var items = [];
+    $.each( data, function( key, val ) {
+      console.log("key: " + key + ", value: " + val);
+      items.push( "<li id='" + key + "'>" + val + "</li>" );
+    });
+
+    $( "<ul/>", {
+      "class": "my-new-list",
+      html: items.join( "" )
+    }).appendTo( "body" );
+  });
+  $.each(myTodos, function(index, item){
+    todoDOM = todoDOM.concat("<li><a href=\"#\" class=\"ui-icon-check todo-item\" data-id=" + item.id + "><span class=\"user\">" +
+    item.user + "</span><span class=\"time\">(" +
+    item.time + "):</span> " + item.description + "</a></li>");
+  });
+}
+
 var app = {
     // Application Constructor
     initialize: function() {
@@ -33,17 +45,10 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-        app.receivedEvent('deviceready');
+
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
     }
 };
